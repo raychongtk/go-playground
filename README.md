f30ff1c9e3e1abb3a5382a50843f52300c23ea27
raychong <raychongtk@gmail.com>
2021-03-27 16:27:01 +0800

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-playground
+
+this project is used to learn go programing language
---
+++ helloworld/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// my first program in GO
+	/* this is comment */
+	fmt.Println("hello world")
+}
