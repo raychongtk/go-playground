f27803d905320cf330b8eaa136329459b167f54a
raychong <raychongtk@gmail.com>
2021-03-28 23:41:27 +0800

    struct method

--- learning/structure/main.go
+++ learning/structure/main.go
@@ -9,10 +9,15 @@ type User struct {
 	email    string
 }
 
+func (user User) login(username string, password string) bool {
+	return username == "ray" && password == "123456"
+}
+
 func main() {
 	user := User{userId: 1, username: "ray", password: "123456", email: "ray@gmail.com"}
 
 	fmt.Println("id =", user.userId)
 	fmt.Println("username =", user.username)
 	fmt.Println("email =", user.email)
+	fmt.Println(user.login(user.username, user.password))
 }
