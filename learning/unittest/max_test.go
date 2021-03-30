8db9de1cd6753e6f5f11ec22688c84fb4e06e3ea
raychong <raychongtk@gmail.com>
2021-03-30 12:00:46 +0800

    helper function

--- learning/unittest/max_test.go
+++ learning/unittest/max_test.go
@@ -1,11 +1,18 @@
 package unittest
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFindMax(t *testing.T) {
 	got := FindMax([]int{0, 12, 3, 4, 5, 62, 7, 8, 29})
 	want := 62
 
+	assertMax(t, got, want)
+}
+
+func assertMax(t *testing.T, got int, want int) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
