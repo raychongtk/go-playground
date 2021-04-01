1b710e883559773a155cdda0677630710ce48458
raychong <raychongtk@gmail.com>
2021-04-01 10:58:10 +0800

    avg

--- playground/main.go
+++ playground/main.go
@@ -74,6 +74,8 @@ func main() {
 	fmt.Println(data)
 	fmt.Println(myArray.PeakIndex(data))
 
+	myMath.AvgWithoutMinMax([]int{48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000})
+
 	myQueue.Add("test")
 	myQueue.Add("test1")
 	myQueue.Add("test2")
---
+++ playground/math/avg.go
@@ -0,0 +1,26 @@
+package math
+
+import (
+	"math"
+)
+
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+func AvgWithoutMinMax(numbers []int) float64 {
+	min := math.MaxInt32
+	max := 0
+	sum := 0
+
+	for _, num := range numbers {
+		if num < min {
+			min = num
+		}
+
+		if num > max {
+			max = num
+		}
+
+		sum += num
+	}
+
+	return float64(sum-min-max) / float64(len(numbers)-2)
+}
