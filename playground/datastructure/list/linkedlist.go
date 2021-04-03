053e367e67505bfa06382d0c3ddc9f9cd5fd1ffa
raychong <raychongtk@gmail.com>
2021-04-03 17:05:04 +0800

    merge linked list

--- playground/datastructure/list/linkedlist.go
+++ playground/datastructure/list/linkedlist.go
@@ -5,6 +5,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// https://leetcode.com/problems/linked-list-cycle/
 func hasCycle(head *ListNode) bool {
 	slow, fast := head, head
 
@@ -18,4 +19,31 @@ func hasCycle(head *ListNode) bool {
 	}
 
 	return false
+}
+
+// https://leetcode.com/problems/merge-in-between-linked-lists/
+func mergeInBetween(list1 *ListNode, a int, b int, list2 *ListNode) *ListNode {
+	var step int
+	n1, n2 := &ListNode{}, &ListNode{}
+	head, head2 := list1, list2
+
+	for head != nil {
+		if step+1 == a {
+			n1 = head
+		}
+		if step == b {
+			n2 = head.Next
+			break
+		}
+		head = head.Next
+		step++
+	}
+
+	n1.Next = list2
+	for head2.Next != nil {
+		head2 = head2.Next
+	}
+	head2.Next = n2
+
+	return list1
 }
