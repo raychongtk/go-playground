9f6b3d4ca3fd8661532825c01a145a8697cf2b8c
ray <raychongtk@gmail.com>
2025-05-20 16:02:24 +0800

    valid parentheses

---
+++ playground/text/valid_parentheses.go
@@ -0,0 +1,27 @@
+package text
+
+func isValid(s string) bool {
+	stack := []string{}
+
+	for _, char := range s {
+		if char == '(' || char == '{' || char == '[' {
+			stack = append(stack, string(char))
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+
+			if char == ')' && stack[len(stack)-1] == "(" {
+				stack = stack[:len(stack)-1]
+			} else if char == '}' && stack[len(stack)-1] == "{" {
+				stack = stack[:len(stack)-1]
+			} else if char == ']' && stack[len(stack)-1] == "[" {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
